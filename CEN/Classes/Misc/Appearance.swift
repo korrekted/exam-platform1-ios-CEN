501c6f70fa8dd67f76fb47678d8d40356ba45490
akonst17 <akonst17@gmail.com>
2022-04-13 16:35:38 +0300

    upgrade; build 8

--- CEN/Classes/Misc/Appearance.swift
+++ CEN/Classes/Misc/Appearance.swift
@@ -18,4 +18,8 @@ struct Appearance {
     static let progress1Color = UIColor(integralRed: 53, green: 56, blue: 163)
     static let progress2Color = UIColor(integralRed: 240, green: 129, blue: 145)
     static let progress3Color = UIColor(integralRed: 244, green: 191, blue: 117)
+    
+    static let successColor = UIColor(integralRed: 46, green: 190, blue: 161)
+    static let errorColor = UIColor(integralRed: 255, green: 115, blue: 99)
+    static let warningColor = UIColor(integralRed: 255, green: 155, blue: 38)
 }
--- CEN/Classes/Service/Stats/Share/StatsShareManager.swift
+++ CEN/Classes/Service/Stats/Share/StatsShareManager.swift
@@ -16,7 +16,7 @@ final class StatsShareManager {
 // MARK: Public
 extension StatsShareManager {
     func sharedContainerURL() -> URL? {
-        FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.cen.widgets")
+        FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.cen.widget")
     }
     
     @discardableResult
